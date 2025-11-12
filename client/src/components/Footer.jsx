b027ecccd105a1743c0b58f733e19b3ce61c428e
Aastha-2304 <aasthasetia.102537@gmail.com>
2025-11-12 18:19:06 +0530

    created footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-50 border-t border-gray-200 py-4">
+      <div className="max-w-7xl mx-auto px-4 text-center text-sm text-slate-600">
+        Made by <span className="font-semibold text-indigo-600">Aastha</span> and{" "}
+        <span className="font-semibold text-indigo-600">Shreshthi</span>
+      </div>
+    </footer>
+  );
+}
